df0a249ce42344394e832898fb9816df7dfdec8f
Karl Fritsche <github@attrib.org>
2018-03-10 11:03:06 +0100

    Fixing travis build

--- client/webpack.config.d/minify.js
+++ client/webpack.config.d/minify.js
@@ -1,17 +1,20 @@
 if( defined.PRODUCTION ) {
-    config.plugins.push(new webpack.optimize.UglifyJsPlugin({
-        compress: {
-            warnings: false,
-            // Disabled because of an issue with Uglify breaking seemingly valid code:
-            // https://github.com/facebookincubator/create-react-app/issues/2376
-            // Pending further investigation:
-            // https://github.com/mishoo/UglifyJS2/issues/2011
-            comparisons: false,
-        },
-        output: {
-            comments: false,
-        },
-        sourceMap: true,
-
-    }));
+    const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+    config.optimization = {};
+    config.optimization.minimizer = [new UglifyJSPlugin({
+        uglifyOptions: {
+            compress: {
+                warnings: false,
+                // Disabled because of an issue with Uglify breaking seemingly valid code:
+                // https://github.com/facebookincubator/create-react-app/issues/2376
+                // Pending further investigation:
+                // https://github.com/mishoo/UglifyJS2/issues/2011
+                comparisons: false
+            },
+            output: {
+                comments: false
+            },
+            sourceMap: true
+        }
+    })];
 }
